4fad0cc801822be4a591abde43495effb2599f79
Tareek Johnson <tareek.johnson@mymona.uwi.edu>
2019-02-02 23:25:46 +0000

    created my first Flask app

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route("/")
-@app.route("/<some_name>")
-def hello(some_name='person'):
-    return "Hello {0}".format(some_name)
+#@app.route("/")
+#@app.route("/<some_name>")
+#def hello(some_name='person'):
+    #return "Hello {0}".format(some_name)
     # return render_template('hello.html', name=some_name)
+    
+@app.route("/")
+def home():
+ return 'My home page' 
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host=os.getenv("IP", '0.0.0.0'), port=int(os.getenv("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=8080) 
