d4dd0f52eaf9e0df06344a636563810577054491
Tareek Johnson <tareek.johnson@mymona.uwi.edu>
2019-02-03 00:00:30 +0000

    created my first Flask app

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
  return 'My home page' 
+ 
+ 
+@app.route("/about")
+def about():
+ return render_template('about.html')
 
 
 if __name__ == "__main__":
